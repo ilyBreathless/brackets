1853989beab1fe4143a9a21972c61fb6e4efd538
ilyBreathless <72036113+ilyBreathless@users.noreply.github.com>
2021-03-13 23:28:06 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let  arrStr = str.split('')
+    for (let i = 0; i<arrStr.length; i++)
+    {
+        for (let j = 0; j < bracketsConfig.length; j++)
+        {
+            if (arrStr[i] == bracketsConfig[j][1] && arrStr[i-1] == bracketsConfig[j][0])
+            {
+                arrStr.splice(i - 1, 2)
+                i = 0
+                break
+            }
+        }
+    }
+        if (arrStr<1) {
+            return true
+        }
+        else
+        {
+            return false
+        }
 }
